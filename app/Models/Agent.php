12a3b23c666111cc29d088bcb83ab63e3283b004
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-12-02 13:46:39 +0300

    Добавлены отношения и fillable поля в модели Agent и User для страницы контрагентов

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Agent extends Model
 {
-    
+    protected $fillable = [
+        'user_id',
+        'title',
+        'inn',
+        'ogrn',
+        'address',
+        'name',
+        'phone',
+        'email',
+        'disabled',
+    ];
+
+    protected $casts = [
+        'disabled' => 'boolean',
+    ];
+
+    /**
+     * Получить пользователя, которому принадлежит контрагент
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Mail;
@@ -65,5 +66,13 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
       return in_array($this->role, ['admin', 'manager'], true);
+    }
+
+    /**
+     * Получить всех контрагентов пользователя
+     */
+    public function agents(): HasMany
+    {
+        return $this->hasMany(Agent::class);
     }
 }
